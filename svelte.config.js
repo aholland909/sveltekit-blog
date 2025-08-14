9bd3a274ec6b781b5c7ffaf4f67fc47ba56f00f4
aholland909 <andrewholland909@gmail.com>
2025-08-14 12:12:58 +0100

    fix: missing prism svelte import

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,6 @@ import 'prismjs/components/prism-typescript.js';
 import 'prismjs/components/prism-javascript.js';
 import 'prismjs/components/prism-json.js';
 import 'prismjs/components/prism-css.js';
-import 'prismjs/components/prism-svelte.js';
 import 'prismjs/components/prism-markdown.js';
 
 /** @type {import('@sveltejs/kit').Config} */
